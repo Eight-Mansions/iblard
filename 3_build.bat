4874f277596030efd7b915361e6dc3751321bbfc
esperknight <esperknight@yahoo.com>
2022-05-27 23:06:42 -0400

    Inserting all the text!

--- 3_build.bat
+++ 3_build.bat
@@ -3,18 +3,50 @@ del /q cd\iblard.bin
 del cd\iblard\EKI\EKI.TXT
 copy trans\EKI.TXT cd\iblard\EKI\EKI.TXT
 
+del cd\iblard\HIKOU\HIKOU.TXT
+copy trans\HIKOU.TXT cd\iblard\HIKOU\HIKOU.TXT
+
+del cd\iblard\LAPUTA01\LAPUTA01.TXT
+copy trans\LAPUTA01.TXT cd\iblard\LAPUTA01\LAPUTA01.TXT
+
+del cd\iblard\LAPUTA02\LAPUTA02.TXT
+copy trans\LAPUTA02.TXT cd\iblard\LAPUTA02\LAPUTA02.TXT
+
+del cd\iblard\MEKIN\MEKIN.TXT
+copy trans\MEKIN.TXT cd\iblard\MEKIN\MEKIN.TXT
+
+del cd\iblard\MORI01\MORI01.TXT
+copy trans\MORI01.TXT cd\iblard\MORI01\MORI01.TXT
+
+del cd\iblard\MORI02\MORI02.TXT
+copy trans\MORI02.TXT cd\iblard\MORI02\MORI02.TXT
+
+del cd\iblard\NINYA2\NINYA2.TXT
+copy trans\NINYA2.TXT cd\iblard\NINYA2\NINYA2.TXT
+
+del cd\iblard\NONA\NONA.TXT
+copy trans\NONA.TXT cd\iblard\NONA\NONA.TXT
+
+del cd\iblard\TEIEN\TEIEN.TXT
+copy trans\TEIEN.TXT cd\iblard\TEIEN\TEIEN.TXT
+
+del cd\iblard\TOMO\TOMO.TXT
+copy trans\TOMO.TXT cd\iblard\TOMO\TOMO.TXT
+
+del cd\iblard\YOZORA\YOZORA.TXT
+copy trans\YOZORA.TXT cd\iblard\YOZORA\YOZORA.TXT
+
+
+
 del font\FNT.COD
 copy font\orig\EKI.COD font\FNT.COD
 tools\iblard_cod_update.exe font\FNT.COD font\letters.txt
 
-
 del font\FNT.FNT
 tools\iblard_font_in.exe font\EKI.FNT.bmp font\FNT.FNT
 
-
 copy exe\orig\SLPS_010.27 exe\SLPS_010.27
 tools\armips.exe code\iblard-assembly.asm
-
 
 del cd\iblard\EKI\EKI.COD
 copy font\FNT.COD cd\iblard\EKI\EKI.COD
